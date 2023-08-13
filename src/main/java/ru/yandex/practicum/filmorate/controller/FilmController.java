d3a3fdc3660d241013c27b02383ec1ea26451cbe
FisanovE <fj1983@mail.ru>
2023-08-13 21:43:00 +0500

    feat: Добавить покрытие тестами классов REST-контроллеров.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,9 +6,11 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import ru.yandex.practicum.filmorate.utils.DateUtils;
 
 import javax.validation.Valid;
 
+import java.time.LocalDate;
 import java.util.*;
 
 
@@ -16,27 +18,52 @@ import java.util.*;
 @Slf4j
 @RestController
 @RequestMapping ("/films")
-	public class FilmController {
+public class FilmController {
 
 	private final Map<Integer, Film> films = new HashMap<>();
 	private int counter = 1;
 
 
 	@PostMapping
-	public Film addNewFilm(@Valid @RequestBody Film film) {
+	public Film addNewFilm(@Valid @RequestBody Film film) throws ValidationException {
 		log.info("Получен запрос к эндпоинту: POST /films");
+		if (film.getName().isBlank()) {
+			throw new ValidationException("Не верный формат названия: \"" + film.getName() + "\"");
+		}
+		if (film.getDescription().length() > 200) {
+			throw new ValidationException("Максимальная длина описания — 200 символов, у вас:  \"" + film.getDescription()
+																										 .length() + "\" символов");
+		}
+		if (film.getReleaseDate().isBefore(LocalDate.parse("1895-12-28", DateUtils.formatter))) {
+			throw new ValidationException("Дата релиза фильма не должна быть раньше 1895.12.28, у вас: \"" + film.getReleaseDate() + "\"");
+		}
+		if (film.getDuration() < 0) {
+			throw new ValidationException("Продолжительность фильма должна быть положительной, у вас:  \"" + film.getDuration());
+		}
 		film.setId(counter);
 		counter++;
 		films.put(film.getId(), film);
 		log.info("Добавлен фильм {}.", film);
 		return films.get(film.getId());
-		//return film;
 	}
 
 
 	@PutMapping
 	public Film updateFilm(@Valid @RequestBody Film film) throws ValidationException {
 		log.info("Получен запрос к эндпоинту: PUT /films");
+		if (film.getName().isBlank()) {
+			throw new ValidationException("Не верный логин: \"" + film.getName() + "\"");
+		}
+		if (film.getDescription().length() > 200) {
+			throw new ValidationException("Максимальная длина описания — 200 символов, у вас:  \"" + film.getDescription()
+																										 .length() + "\" символов");
+		}
+		if (film.getReleaseDate().isBefore(LocalDate.parse("1895-12-28", DateUtils.formatter))) {
+			throw new ValidationException("Дата релиза фильма не должна быть раньше 1895.12.28, у вас: \"" + film.getReleaseDate() + "\"");
+		}
+		if (film.getDuration() < 0) {
+			throw new ValidationException("Продолжительность фильма должна быть положительной, у вас:  \"" + film.getDuration());
+		}
 		for (Film currentFilm : films.values()) {
 			if (currentFilm.getName().equals(film.getName()) && currentFilm.getReleaseDate()
 																		   .equals(film.getReleaseDate())) {
@@ -46,7 +73,6 @@ import java.util.*;
 		}
 		log.info("Изменён фильм {}.", film);
 		return films.get(film.getId());
-		//return film;
 	}
 
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,16 +3,12 @@ package ru.yandex.practicum.filmorate.controller;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exeptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 
 import javax.validation.Valid;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 
-//@Controller
 @Slf4j
 @RestController
 @RequestMapping ("/users")
@@ -21,39 +17,56 @@ public class UserController {
 	private int counter = 1;
 
 	@PostMapping
-	public User addNewUser(@Valid @RequestBody User user) {
+	public User addNewUser(@Valid @RequestBody User user) throws ValidationException {
 		log.info("Получен запрос к эндпоинту: POST /users");
-		if (user.getName() == null || user.getName().isEmpty()) {
+		if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+			throw new ValidationException("Не верный формат электронной почты: \"" + user.getEmail() + "\"");
+		}
+		if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+			throw new ValidationException("Не верный логин: \"" + user.getLogin() + "\"");
+		}
+		if (user.getName() == null || user.getName().isBlank()) {
 			user.setName(user.getLogin());
+		}
+		if (user.getBirthday().isAfter(LocalDate.now())) {
+			throw new ValidationException("Дата рождения не может быть в будущем: \"" + user.getBirthday() + "\"");
 		}
 		user.setId(counter);
 		counter++;
 		users.put(user.getId(), user);
 		log.info("Добавлен пользователь {}.", user);
 		return users.get(user.getId());
-		//return user;
 	}
 
 	@PutMapping
 	public User updateUser(@Valid @RequestBody User user) throws ValidationException {
 		log.info("Получен запрос к эндпоинту: PUT /users");
-		if (user.getName() == null || user.getName().isEmpty()) {
+		if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+			throw new ValidationException("Не верный формат электронной почты: \"" + user.getEmail() + "\"");
+		}
+		if (user.getLogin().isBlank()) {
+			throw new ValidationException("Не верный логин: \"" + user.getLogin() + "\"");
+		}
+		if (user.getName().isBlank()) {
 			user.setName(user.getLogin());
 		}
-		for (User us : users.values()) {
-			if (us.getEmail().equals(user.getEmail())) {
-				throw new ValidationException("Пользователь с электронной почтой " + user.getEmail() + " уже зарегистрирован.");
+		if (user.getBirthday().isAfter(LocalDate.now())) {
+			throw new ValidationException("Дата рождения не может быть в будущем: \"" + user.getBirthday() + "\"");
+		}
+		for (User currentUser : users.values()) {
+			if (currentUser.getEmail().equals(user.getEmail())) {
+				throw new ValidationException("Этот email уже зарегистрирован: \"" + user.getEmail() + "\"");
 			}
 		}
+
 		users.put(user.getId(), user);
 		log.info("Изменён пользователь {}.", user);
 		return users.get(user.getId());
-		//return user;
 	}
 
 	@GetMapping
 	public Collection<User> getAllUsers() {
-		return users.values();
+		return new ArrayList<>(users.values());
 	}
 
 }
