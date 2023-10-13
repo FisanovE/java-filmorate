2dd3409debfb0382ada39605ab1a4775139a13e5
IgorVasilev <777igorvasiliev@gmail.com>
2023-10-13 19:24:59 +0300

    fix: checkstyle by Igor PC format

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/FilmRowMapper.java
@@ -9,12 +9,11 @@ import java.util.Objects;
 
 public class FilmRowMapper implements RowMapper<Film> {
 
-	@Override
-	public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
-		return Film.builder().id(rs.getLong("film_id")).name(rs.getString("name"))
-				   .description(rs.getString("description"))
-				   .releaseDate(Objects.requireNonNull(rs.getDate("release_date")).toLocalDate())
-				   .duration(rs.getInt("duration")).
-				build();
-	}
+    @Override
+    public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return Film.builder().id(rs.getLong("film_id")).name(rs.getString("name"))
+                .description(rs.getString("description"))
+                .releaseDate(Objects.requireNonNull(rs.getDate("release_date")).toLocalDate())
+                .duration(rs.getInt("duration")).build();
+    }
 }
