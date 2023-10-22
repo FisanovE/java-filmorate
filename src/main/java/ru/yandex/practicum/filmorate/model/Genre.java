3b2db110e148b45003c9948bdea9f6e57adcff8f
IgorVasilev <777igorvasiliev@gmail.com>
2023-10-22 19:44:44 +0300

    fix: переопределить equals и hashcode по id для сущности genre

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -2,10 +2,27 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
 @Builder
 @ToString
 public class Genre {
     private Long id;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Genre)) return false;
+        Genre genre = (Genre) o;
+        return Objects.equals(getId(), genre.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
