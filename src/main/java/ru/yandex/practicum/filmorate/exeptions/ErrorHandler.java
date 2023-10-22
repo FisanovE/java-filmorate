4f56f0e02ccbbcbafaf4794f915aee859a1177ad
IgorVasilev <777igorvasiliev@gmail.com>
2023-10-22 21:07:10 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exeptions/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeptions/ErrorHandler.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Arrays;
-
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
